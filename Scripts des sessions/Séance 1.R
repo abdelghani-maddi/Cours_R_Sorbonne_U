c650169a6bbee704e88c71cc6b82bc17ec2377aa
Abdelghani Maddi <maddiabdelghani@gmail.com>
2023-01-19 15:45:38 +0100

    Commit 19/01/2023 15h45

--- Scripts des sessions/Séance 1.R
+++ Scripts des sessions/Séance 1.R
@@ -227,7 +227,6 @@ sexe[c(1, 3, 4)] <- c("Homme", "Homme", "Homme")
 sexe[c(1, 3, 4)] <- "Homme"
 
 #### LISTES ET TABLEAUX DE DONNEES
-<<<<<<< HEAD
  # LES LISTES :
     # Par nature, les vecteurs ne peuvent contenir que des valeurs de même type 
     # (numérique, textuel ou logique). Or, on peut avoir besoin de représenter des 
